f12dcf69420f8f220cae804ed1668c332a968e95
Smart Developer <48024874+smartdev0612@users.noreply.github.com>
2023-01-17 03:55:42 +0000

    Calcuate O(n^2)

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('Hello world')
+ /*const large = new Array(100).fill('nemo');
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+  console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
+}
+
+findNemo(large);
+*/
+
+// Log all pairs of array
+const boxes = [1, 2, 3, 4, 5];
+
+function logAllPairsOfArray(boxes) {
+  for (let i = 0; i < boxes.length; i++) {
+    for (let j = 0; j < boxes.length; j++) {
+      console.log(i, j);
+    }
+  }
+}
+
+logAllPairsOfArray(boxes);
