bcaefc0f483b92985d464a02cb275a30c10277b8
Smart Developer <48024874+smartdev0612@users.noreply.github.com>
2023-01-25 02:42:02 +0000

    Create Linked List class

--- main.js
+++ main.js
@@ -1,26 +1,168 @@
- /*const large = new Array(100).fill('nemo');
+/*const large = new Array(100).fill('nemo');
 
 function findNemo(array) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 'nemo') {
-      console.log('Found NEMO!');
-    }
-  }
-  console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
+ for (let i = 0; i < array.length; i++) {
+   if (array[i] === 'nemo') {
+     console.log('Found NEMO!');
+   }
+ }
+ console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
 }
 
 findNemo(large);
 */
 
 // Log all pairs of array
-const boxes = [1, 2, 3, 4, 5];
+// const boxes = ['a', 'b', 'c', 'd', 'e'];
 
-function logAllPairsOfArray(boxes) {
-  for (let i = 0; i < boxes.length; i++) {
-    for (let j = 0; j < boxes.length; j++) {
-      console.log(i, j);
+// function logAllPairsOfArray(boxes) {
+//   for (let i = 0; i < boxes.length; i++) {
+//     for (let j = 0; j < boxes.length; j++) {
+//       console.log(boxes[i], boxes[j]);
+//     }
+//   }
+// }
+
+// logAllPairsOfArray(boxes);
+
+/*class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
     }
+    delete this.data[this.length - 1];
+    this.length--;
   }
 }
 
-logAllPairsOfArray(boxes);
+const newArray = new MyArray();
+newArray.push("hi");
+newArray.push('you');
+newArray.push('are');
+newArray.push('nice');
+newArray.delete(1);
+console.log(newArray);
+*/
+
+// 10 --> 5 --> 16
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+    return array;
+  }
+
+  insert(index, value) {
+    // check params
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    // check params
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  remove(index) {
+    // check params
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+myLinkedList.printList();
+myLinkedList.remove(2);
+console.log(myLinkedList);
