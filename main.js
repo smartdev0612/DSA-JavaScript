68951f6454e7b6b01bb66cff7e570009858e01b9
Smart Developer <48024874+smartdev0612@users.noreply.github.com>
2023-02-01 14:08:22 +0000

    Merge Sort

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ console.log(newArray);
 */
 
 // 10 --> 5 --> 16
-class Node {
+/*class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
@@ -166,3 +166,487 @@ myLinkedList.insert(20, 88);
 myLinkedList.printList();
 myLinkedList.remove(2);
 console.log(myLinkedList);
+*/
+
+/*
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+    this.length++;
+    return this.printList();
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+    return this.printList();
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+    return array;
+  }
+
+  insert(index, value) {
+    // check params
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const follower = leader.next;
+    leader.next = newNode;
+    newNode.prev = leader;
+    newNode.next = follower;
+    follower.prev = newNode;
+    this.length++;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    // check params
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  remove(index) {
+    // check params
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+
+  reverse() {
+    if (!this.head.next) {
+      return this.head;
+    }
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while(second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this.printList();
+  }
+}
+
+const myLinkedList = new DoublyLinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(1, 99);
+myLinkedList.insert(20, 88);
+// myLinkedList.printList();
+myLinkedList.remove(2);
+myLinkedList.reverse();
+console.log(myLinkedList);
+
+*/
+
+/*
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.array = [];
+  }
+
+  peek() {
+    return this.array[this.array.length - 1];
+  }
+
+  push(value) {
+    this.array.push(value);
+    return this;
+  }
+
+  pop() {
+    this.array.pop();
+    return this;
+  }
+}
+
+const myStack = new Stack();
+myStack.push('google');
+myStack.push('udemy');
+myStack.push('discord');
+myStack.peek();
+myStack.pop();
+myStack.pop();
+myStack.pop();
+console.log(myStack);
+*/
+
+/*
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if(!this.first) {
+      return null;
+    }
+    if(this.first === this.last) {
+      this.last = null;
+    }
+    const holdingPointer = this.first;
+    this.first = this.first.next;
+    this.length--;
+    return holdingPointer;
+  }
+}
+
+const myQueue = new Queue();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.enqueue('Samir');
+myQueue.peek();
+myQueue.dequeue();
+// myQueue.peek();
+
+*/
+
+// Binary Tree
+/*class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while(true) {
+        if (value < currentNode.value) {
+          // Left
+          if(!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left
+        } else {
+          // Right
+          if(!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  
+  lookup(value) {
+    if(!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while(currentNode) {
+      if(value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return false;
+  }
+
+  // remove
+  remove(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    let parentNode = null;
+    while(currentNode) {
+      if(value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (value === currentNode.value) {
+        // Option 1: No right child
+        if (currentNode.right === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left;
+          } else {
+            // if parent > current value, make current left child a child of parent
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left;
+            }
+            // if parent < current value, make left child a right child of parent
+            else if(currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left;
+            }
+          }
+        }
+        // Option 2: Right child which doesn't have a left child
+        else if (currentNode.right.left === null) {
+          if(parentNode === null) {
+            this.root = currentNode.left;
+          } else {
+            currentNode.right.left = currentNode.left;
+
+            // if parent > current, make right child of the left the parent
+            if(currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right;
+            }
+            // if parent < current, make right child a right child of the parent
+            else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.right;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+tree.lookup(9);
+console.log(JSON.stringify(traverse(tree.root)));
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
+*/
+
+// Graph
+/*class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
+
+  addVertex(node) {
+    
+  }
+
+  addEdge(node1, node2) {
+    //undirected graph
+  }
+
+  showConnections() {
+    
+  }
+}
+
+const myGraph = new Graph();
+myGraph.addVertex('0');
+myGraph.addVertex('1');
+myGraph.addVertex('2');
+myGraph.addVertex('3');
+myGraph.addVertex('4');
+myGraph.addVertex('5');*/
+/*
+function findFactorialRecursive(number) {
+  if (number === 2) {
+    return 2;
+  }
+  return number * findFactorialRecursive(number - 1);
+}
+
+function findFactorialIterative(number) {
+  let answer = 1;
+  if (number === 2) {
+    answer = 2;
+  }
+  for (let i = 2; i <= number; i++) {
+    answer *= i;
+  }
+  return answer;
+}
+
+console.log(findFactorialRecursive(5));
+*/
+
+/* function reverseString(str) {
+  let str_array = str.split("");
+  let reversedString = [];
+  function addString(array) {
+    if(array.length > 0) {
+      reversedString.push(array.pop());
+      addString(array);
+    }
+    return;
+  }
+  addString(str_array);
+  return reversedString.join("");
+}
+
+console.log(reverseString('yoyo mastery')); */
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    // set current index as mininum
+    let min = i;
+    let temp = array[i];
+    for (let j = i + 1; j < length; j++) {
+      if (array[j] < array[min]) {
+        // update minimum if current is lower that what we had previously
+        min = j;
+      }
+    }
+    array[i] = array[min];
+    array[min] = temp;
+  }
+  return array;
+}
+
+
+function insertionSort(array) {
+  let length = array.length;
+  for (let i = 0; i < length; i++) {
+    if (array[i] < array[0]) {
+      // move number to the first position
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      for (let j = 0; j < i; j++) {
+        if (array[i] > array[j - 1] && array[i] < array[j]) {
+          // move number to the right spot
+          array.splice(j, 0, array.splice(i, 1)[0]);
+        }
+      }
+    }
+  }
+  return array;
+}
+
+//console.log(insertionSort(numbers));
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array
+  }
+  // Split Array in into  right and left
+  const length = array.length;
+  const middle = Math.floor(length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+  
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
+}
+
+function merge(left, right) {
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(leftIndex < left.length && rightIndex < right.length) {
+    if(left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const answer = mergeSort(numbers);
+console.log(answer);
